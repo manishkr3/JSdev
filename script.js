b2ce35596dd02034737f25218e8cb08c6ec1cd83
manishkr3 <manishmpib@gmail.com>
2021-08-26 15:07:49 -0700

    26 Aug

---
+++ script.js
@@ -0,0 +1,80 @@
+// let js = "amazing"
+// console.log(234 / 77)
+// console.log(22 / 7);
+// console.log("Manish Kumar")
+// let first = "You can do it Bro! just try harder"
+// console.log(first);
+// let firstname = "Manish and we cant use here name its kind of reserved"
+// console.log(firstname);
+// console.log("keywords eg: $amk,_and,nAme,andnotstartwithnumber");
+// let Amnst = "tatar";
+// console.log(Amnst);
+// console.log("but its not the professional way mean its not the convention");
+// // we use it for oop and some other purposes
+// //also their is convension of const defining with all capital
+// let PI = 3.14;
+// let myFirstName = 'Manishkr';
+// console.log(myFirstName);
+// console.log(myFirstName)
+// console.log(myFirstName);
+// //it has Dynamic typing , means it automatically define the data type to the value stored in the
+// //variable.(note : that value has the data type not the Variable)
+// *******************************************************************************
+/*//12
+let javaScriptIs = true;
+console.log(javaScriptIs);
+console.log(typeof javaScriptIs);
+console.log(typeof true);
+console.log(typeof 34);
+console.log(typeof 'Hare Krishna');
+let krish;//declaration
+console.log(krish);
+console.log(typeof krish);
+age = 34;//re
+age = 34;//re
+krish = 34;//definition
+console.log(krish);
+console.log(typeof krish);
+krish = 'Om Namah Shivay'
+console.log(krish);
+console.log(typeof krish);
+let om;//declaration
+console.log(om);
+console.log(typeof om);
+console.log(null);
+console.log(typeof null);//maybe its bug kept for legacy reasons ,it outputs object but it
+//should output null as undefined incase of only declared variable
+***************************************************************************/
+//13
+/*let age = 23;
+age = 34;//reassigning the age variable or  mutating the age variable
+const sex = "male";
+//sex = 'female'; it throws an error and thus "Immutable"
+// const dateOfBirth; this also throws an error you do need to initialize it at beginning
+var age = 34;
+age = 35;//although its the same as but you should always prefer let and not use the
+//because let is block scoped while var is function scoped
+//!also always use the let not just age=34; although it is errorless but not efficient
+*/
+//14 basic operators
+//math operators
+const now = 2044;//watch how i am using const ,cause its not meant to change
+const ageRama = now - 2002;//be as descriptive as u can
+const ageRadha = now - 2018;//since 2044 is repeating itself use a variable instead
+console.log(ageRadha, ageRama);
+console.lo
+g(ageRama / 23, ageRama + 234, 2 ** 3);
+//2**3 means 2 to the power 3 =2*2*2;
+const firstName = 'Hare';
+const lastName = 'Krishna';
+console.log(firstName + ' ' + lastName);//concatenation
+//assignment operators
+let x = 4 + 5;//assignment and addition operator
+console.log(x);
+x += 10;
+console.log(x);
+x *= 10;//x=x*10
+x++; //x=x+1;
+x--;//x=x-1
+//comparison operators
+
